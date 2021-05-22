9caf0015e9ca295a68d35a47cb64877c977cd8e4
Saket Upadhyay <saketupadhya@gmail.com>
2021-05-22 12:05:23 +0530

    JSON response handler

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 from flask import Flask
-
+from flask import request, render_template
 app = Flask(__name__)
+import json
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'LightHouse API : Open Endpoints -> []'
+
+
+@app.route('/getinit',methods=['POST'])
+def getinit():
+    if request.method == 'GET':
+        activatebool=request.args.get('activate')
+        if activatebool == "true":
+            # SEND REQUEST TO APP
+                print("APP REQ")
+        else:
+            print("NO REQ")
+    if request.method == 'POST':
+
+
+        request_data = request.get_json()
+        userid = request_data['uid']
+        initCode = request_data['initCode']
+        initHash = request_data['inithash']
+        userid = request_data['userid']
+        DATARET = contactsmp(initCode, initHash, userid)
+        DATARET={'status':'true'}
+        return json.dumps(DATARET,indent=4)
+
+    return "OK"
+
+
+def contactsmp(initCode,initHash,userid):
+    RESULT=""
+    # Send request to phone by searching DB for userID
+    return RESULT
 
 
 if __name__ == '__main__':
