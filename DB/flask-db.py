be79dd101260242bed43132bf92e84bff3e7b91a
Anushka Doshi <53192647+anushkadoshi21@users.noreply.github.com>
2021-05-25 01:53:10 +0530

    Update flask-db.py

--- DB/flask-db.py
+++ DB/flask-db.py
@@ -7,6 +7,13 @@
 # Stored Procedures and their use has been mentioned in sql files!
 # This file will be ready to use with args actually taking the real time inputs and then responding in real time
 
+
+# EDIT ---->
+# 2 routes have been added for extracting details and Authentication
+# /details for fetching details given the email_id (userid)
+# /auth2 for authentication of user based on userid and hashcode
+# /insert to insert the captcha in captcha table
+
 from flask import Flask,render_template,request
 from flask_mysqldb import MySQL
 
@@ -29,6 +36,26 @@ def default_Route():
     mysql.connection.commit()
     cursor.close()
     return 'Hello World'
+
+@app.route('/details')
+def fetch_details():
+    cursor = mysql.connection.cursor()
+    args = ('abc@gmail','_','_')
+    cursor.callproc('extract_details',args)
+    cursor.execute('SELECT @_extract_details_1,@_extract_details_2')
+    dd = cursor.fetchall() #dd[0][0] is Hash (password) and dd[0][1] is Captcha (initcode)
+    print(dd) #dd[0][0]='1' signifies timeout
+    return 'extract world'
+
+@app.route('/auth2')
+def validate_User():
+    cursor = mysql.connection.cursor()
+    args = ('abc@gmail','ab234rt',1)
+    cursor.callproc('AuthenticateUser2',args)
+    cursor.execute('SELECT @_AuthenticateUser2_2')
+    dd = cursor.fetchall()
+    print(dd) #0=Success 1=Failure
+    return 'Auth2 world'
 
 @app.route('/auth')
 def authenticate_Generate():
