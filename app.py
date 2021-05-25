155c1543c1463f44c710ba2c7080293c4bc2249b
Saket Upadhyay <saketupadhya@gmail.com>
2021-05-25 20:08:41 +0530

    User check and validation functions added

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
 from flask import Flask
 from flask import request, render_template
 import json
-
+import time
+import csv
 app = Flask(__name__)
 
+global AppRequest
+global UIDmanager
+UIDmanager={'active':False,'uid':''}
+AppRequest={'uid':None}
 
+
+def contactdb(initHash, userid):
+    global AppRequest
+    global UIDmanager
+    setvaliduser=False
+    # check userid in database
+    with open('DB/data.csv','r') as datsv:
+        csvr=csv.reader(datsv,delimiter=',')
+        line_count=0
+        for row in csvr:
+            if row[0] == userid and row[1] == initHash:
+                setvaliduser=True
+            else:
+                setvaliduser=False
+
+    #set uid activated to True
+    if setvaliduser:
+        AppRequest.update({'uid':userid})
+        # Send request to phone by searching DB for userID
+        time.sleep(4.0)
+        if UIDmanager.get('uid') == AppRequest.get('uid'):
+            if UIDmanager.get('active') == True:
+                return True
+        else:
+            return False
+    else:
+        return False
+
+
+
+#Update the Android Request to Global Paramaters
+def testActivationFunction(activatedata):
+    global AppRequest
+    global UIDmanager
+    if "true" in activatedata:
+        if activatedata in AppRequest.get('uid'):
+            tempdict={'active':True,'uid':AppRequest.get('uid')}
+            UIDmanager.update(tempdict)
+            return "STORED"
+        else:
+            return "NO REQ"
+    else:
+        tempdict = {'active':False, 'uid': AppRequest['uid']}
+        UIDmanager.update(tempdict)
+        return "NO REQ"
 
 @app.route('/')
 def hello_world():
@@ -12,15 +62,15 @@ def hello_world():
 
 @app.route('/getinit', methods=['GET','POST'])
 def getinit():
+    global AppRequest
+    global UIDmanager
+    UIDmanager = {'active': False, 'uid': ''}
+    AppRequest = {'uid': ''}
     if request.method == 'GET':
-        activatebool = request.args.get('activate')
-        if activatebool == "true":
-            # SEND REQUEST TO APP
-            print("APP REQ")
-            return "APP REQ"
-        else:
-            print("NO REQ")
-            return "NO REQ"
+        activatedata = request.args.get('activate')
+
+        return testActivationFunction(activatedata)
+
     if request.method == 'POST':
 
         try:
@@ -28,19 +78,15 @@ def getinit():
             initCode = request_data['initCode']
             initHash = request_data['inithash']
             userid = request_data['userid']
-            DATARET = contactsmp(initCode, initHash, userid)
+            DATARET = contactdb(initHash, userid)
 
-            return json.dumps({'status': 'true', 'DATA': DATARET}, indent=4)
+            return json.dumps({'status': DATARET, 'initcode': initCode}, indent=4)
         except Exception:
-            return json.dumps({'status': 'error'}, indent=4)
+            return json.dumps({'status': 'ERROR'}, indent=4)
 
     return "OK"
 
 
-def contactsmp(initCode, initHash, userid):
-    RESULT = str(initCode)+";"+str(initHash)+";"+str(userid)
-    # Send request to phone by searching DB for userID
-    return RESULT
 
 
 if __name__ == '__main__':
