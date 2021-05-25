77c5cbc643064a70c6a3270c68e9d7d367059716
Saket Upadhyay <saketupadhya@gmail.com>
2021-05-25 22:20:36 +0530

    Working prototype API v1.0

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request, render_template
 import json
+from flask import jsonify
 import time
 import csv
 app = Flask(__name__)
@@ -29,7 +30,13 @@ def contactdb(initHash, userid):
     if setvaliduser:
         AppRequest.update({'uid':userid})
         # Send request to phone by searching DB for userID
+        print("WAITING FOR USER")
+        # print(AppRequest.get('uid'))
         time.sleep(4.0)
+        print("== UID STAT == ")
+        print(UIDmanager.get('uid'))
+        print(UIDmanager.get('active'))
+        print("== UID STAT EOS == ")
         if UIDmanager.get('uid') == AppRequest.get('uid'):
             if UIDmanager.get('active') == True:
                 return True
@@ -42,10 +49,13 @@ def contactdb(initHash, userid):
 
 #Update the Android Request to Global Paramaters
 def testActivationFunction(activatedata):
+    print("==== testActivation Called ====")
     global AppRequest
     global UIDmanager
-    if "true" in activatedata:
-        if activatedata in AppRequest.get('uid'):
+    print(AppRequest.get('uid'))
+    if activatedata[0] == 'true' :
+        print(AppRequest.get('uid'))
+        if activatedata[1] in AppRequest.get('uid'):
             tempdict={'active':True,'uid':AppRequest.get('uid')}
             UIDmanager.update(tempdict)
             return "STORED"
@@ -54,7 +64,7 @@ def testActivationFunction(activatedata):
     else:
         tempdict = {'active':False, 'uid': AppRequest['uid']}
         UIDmanager.update(tempdict)
-        return "NO REQ"
+        return "NO REQ 2"
 
 @app.route('/')
 def hello_world():
@@ -71,22 +81,30 @@ def getinit():
     UIDmanager = {'active': False, 'uid': ''}
     AppRequest = {'uid': ''}
     if request.method == 'GET':
-        activatedata = request.args.get('activate')
-
+        activatedata=[]
+        activatedata.append(request.args.get('activate'))
+        activatedata.append(request.args.get('uid'))
+        print(activatedata)
+        AppRequest.update({'uid': activatedata[1]})
         return testActivationFunction(activatedata)
 
     if request.method == 'POST':
-
         try:
             request_data = request.form.to_dict()
             initCode = request_data['initCode']
             initHash = request_data['inithash']
             userid = request_data['userid']
+
+            print(userid)
+            print(initHash)
+
             DATARET = contactdb(initHash, userid)
 
-            return json.dumps({'status': DATARET, 'initcode': initCode}, indent=4)
+            RET={'status': DATARET, 'initcode': int(initCode)}
+            return jsonify(RET)
         except Exception:
-            return json.dumps({'status': 'ERROR'}, indent=4)
+            RET={'status': 'ERROR'}
+            return jsonify(RET)
 
     return "OK"
 
@@ -94,4 +112,4 @@ def getinit():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,host='0.0.0.0', port=5000, threaded=True)
